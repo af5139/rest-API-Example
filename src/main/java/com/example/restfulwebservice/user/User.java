47ea7c8bb078aafaee9526584e61c51af3f9a589
woong <gi5060@naver.com>
2022-09-22 10:24:26 +0900

    rest API

---
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -0,0 +1,44 @@
+package com.example.restfulwebservice.user;
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.BatchSize;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonFilter("UserInfo")
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Size(min=2,message = "Name은 2글자 이상 입력해 주세요.")
+    private String name;
+    @Past
+    private Date joinDate;
+
+    //@JsonIgnore
+    private String password;
+    //@JsonIgnore
+    private String ssn;
+
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts;
+
+    public User(int i, String kenneth, Date date, String pass1, String s) {
+    }
+}
---
+++ src/main/java/com/example/restfulwebservice/user/UserDaoService.java
@@ -0,0 +1,66 @@
+package com.example.restfulwebservice.user;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+@Service
+public class UserDaoService {
+    private static List<User> users = new ArrayList<>();
+
+    private static int usersCount=3;
+
+    static{
+        users.add(new User(1, "Kenneth", new Date(),"pass1","701010-1111133" ));
+        users.add(new User(2, "Alice", new Date(),"pass2","801010-1122244"));
+        users.add(new User(3, "Elena", new Date(),"pass3","901010-1232155"));
+    }
+
+    public List<User> findAll(){
+        return users;
+    }
+
+    public User save(User user){
+        if(user.getId() == null){
+            user.setId(++usersCount);
+        }
+
+        users.add(user);
+        return user;
+    }
+
+    public User findOne(int id){
+        for(User user : users){
+            if(user.getId() == id){
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public User deleteById(int id){
+        Iterator<User> iterator=users.iterator();
+
+        while(iterator.hasNext()){
+            User user = iterator.next();
+
+            if(user.getId() == id){
+                iterator.remove();;
+                return user;
+            }
+        }
+
+        return null;
+    }
+
+    public void modifyById(User user){
+        for(User us : users){
+            if(us.getId()==user.getId()){
+                us.setName(user.getName());
+            }
+        }
+    }
+}
